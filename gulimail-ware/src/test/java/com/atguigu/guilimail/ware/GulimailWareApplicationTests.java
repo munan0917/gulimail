bda5db09f4568dbea26465ee2fb3f1807a56c7c3
zqn <zqn0917@163.com>
2022-12-17 16:24:53 +0800

    项目初始结构创建

---
+++ gulimail-ware/src/test/java/com/atguigu/guilimail/ware/GulimailWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.guilimail.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
